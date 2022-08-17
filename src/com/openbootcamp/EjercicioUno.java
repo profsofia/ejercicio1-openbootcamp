4ffff9b93b227ca59f1385cf45dcd54d105cb61e
sofia <sofiainesschenone@gmail.com>
2022-08-17 19:46:45 -0300

    se creó el paquete y la clase

---
+++ src/com/openbootcamp/EjercicioUno.java
@@ -0,0 +1,11 @@
+package com.openbootcamp;
+
+public class EjercicioUno {
+    /*
+    * atajo para crear el método de arranque
+    * main
+    * */
+    public static void main(String[] args) {
+
+    }
+}
